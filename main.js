050afe343071e0370a3663fcde905799991e407d
VincenzoVitello <92366183+VincenzoVitello@users.noreply.github.com>
2021-12-09 16:57:56 +0100

    progetto finito funzionante

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+const open = document.querySelector(".fa-bars");
 const hamburger = document.querySelector(".hamburger-menu");
+const close = document.querySelector(".fa-times");
 
-hamburger.addEventListener('click', function(){
+open.addEventListener('click', function(){
     // qui ci scrivi quello che deve accadere al click
-  
-   );
+   hamburger.classList.add('active');
+}
+);
+close.addEventListener('click', function(){
+    // qui ci scrivi quello che deve accadere al click
+   hamburger.classList.remove('active');
+}
+);
